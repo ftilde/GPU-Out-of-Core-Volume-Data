432a8ebd8b02eb48609aba7feb7c0d4a056e533d
Dominik Drees <dominik.drees@uni-muenster.de>
2025-05-09 11:05:35 +0200

    HACK: Enable compilation on ubuntu 24.04
    See:
    
    _Float16:
    https://forums.developer.nvidia.com/t/including-cub-header-breakes-compilation-with-gcc-12-and-sse2-or-better/255018
    
    __builtin_ia32_...:
    https://stackoverflow.com/questions/78271025/why-is-nvcc-giving-the-error-identifier-builtin-ia32-ldtilecfg-is-undefined

--- Applications/DVR-ray-caster/include/VolumeRayCaster.hpp
+++ Applications/DVR-ray-caster/include/VolumeRayCaster.hpp
@@ -2,7 +2,16 @@
 
 #include <GcCore/libCommon/CppNorm.hpp>
 #include <GcCore/cuda/libGPUCache/CacheManager.hpp>
+#undef _Float16
 #include <GcCore/libData/MetaData.hpp>
+
+#ifndef __builtin_ia32_ldtilecfg 
+#define __builtin_ia32_ldtilecfg exit(123);
+#endif
+
+#ifndef __builtin_ia32_sttilecfg
+#define __builtin_ia32_sttilecfg exit(123);
+#endif
 
 namespace tdns
 {
